a14c38c4f92a706cf5d48be065c347bafb6f1060
AnderSon <son@lincolnix.net>
2016-05-12 00:54:18 -0500

    minor

---
+++ public/assets/home-4f2001c2dcb38e26fef46d7a4186b2c2bb82ba92874c015f1a1d8c72ea137031.js
@@ -0,0 +1,15 @@
+/*var lock = new Auth0Lock("TOJl4WP8HvCG9l5BSgimyX9NztJ0xC2T", "app50531416.auth0.com");
+var callback = "http://localhost:3000/auth/auth0/callback";
+
+function signin() {
+    lock.show({
+        callbackURL: "http://localhost:3000/auth/auth0/callback",
+        //callbackURL: callback,
+        responseType: 'code', 
+        authParams: {
+            scope: 'openid name email picture'
+        }
+    });
+    }*/
+
+// temporarily moving code to view level
